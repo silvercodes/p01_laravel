3e5444383e021c8ae7c7f51b6bc054aed547f35b
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-01-23 18:54:31 +0300

    add: relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Product extends Model
 {
@@ -16,4 +18,9 @@ class Product extends Model
     protected $hidden = [
         'created_at', 'updated_at',
     ];
+
+    public function subCategory(): BelongsTo
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
 }
--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class SubCategory extends Model
 {
@@ -16,4 +18,14 @@ class SubCategory extends Model
     protected $hidden = [
         'created_at', 'updated_at',
     ];
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
